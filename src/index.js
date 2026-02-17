5d5efc8993b79913a3e14e105cca59f74677236b
harpreetkhatter <khatterharpreet@gmail.com>
2026-02-17 16:07:17 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import express from "express"
+import "dotenv/config"
+import authRoutes from "./routes/authRoutes.js"
+import bookRoutes from "./routes/bookRoutes.js"
+import { connectDB } from "./lib/db.js"
+
+import cors from "cors"
+
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+app.use("/api/auth",authRoutes)
+app.use("/api/books",bookRoutes)
+
+const PORT = process.env.PORT || 3000
+
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`)
+  connectDB()
+})
